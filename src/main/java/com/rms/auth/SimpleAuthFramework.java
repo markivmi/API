737a31cb31a74da266d0feed548185e61e89f7d9
babbupandey <babbupandey@gmail.com>
2014-08-04 23:04:33 -0700

    Added stubs for Authentication framework and service interception.

--- src/main/java/com/rms/auth/SimpleAuthFramework.java
+++
@@ -1,7 +0,0 @@
-package com.rms.auth;
-
-/**
- * Created by vpandey on 8/4/2014.
- */
-public class SimpleAuthFramework {
-}
---
+++ src/main/java/com/rms/interceptor/RmsInterceptor.java
@@ -0,0 +1,15 @@
+package com.rms.interceptor;
+
+import com.sun.jersey.spi.container.ContainerRequest;
+import com.sun.jersey.spi.container.ContainerRequestFilter;
+
+/**
+ * Created by Vinay on 8/4/2014.
+ */
+
+public class RmsInterceptor implements ContainerRequestFilter {
+    @Override
+    public ContainerRequest filter(ContainerRequest containerRequest) {
+        return null;
+    }
+}
